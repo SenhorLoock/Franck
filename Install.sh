dcd408c1c37ba30f0f6fed177b3f93aae661c02c
SenhorLoock <71995271+shoowl@users.noreply.github.com>
2020-11-23 15:17:15 -0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,9 +1,36 @@
 clear 
 
+figlet -f mono12 "1Etapa" | lolcat
+
 git clone https://github.com/shoowl/Auxiliar
+echo "Precione Enter Para Continua" | lolcat
+read -s Enter1
+cd Auxiliar
+bash install.sh
+
+clear
+figlet -f mono12 "2Etapa" | lolcat
+
 git clone https://github.com/sundowndev/PhoneInfoga
+echo "Precione Enter Para Continuar" | lolcat
+read-s Enter2
+
+clear
+figlet -f mono12 "2Etapa" | lolcat
 git clone https://github.com/Cesar-Hack-Gray/SocialSploit
+echo " Precione Enter Para Continuar" | lolcat
+read -s Enter3
+cd SocialSploit 
+
+bash install.sh
+
+clear
+figlet -f mono12 "4Etapa" | lolcat
 git clone https://github.com/Cesar-Hack-Gray/webcam
+echo "Precione Enter Para Continuar" | lolcat
+read -s Enter4
+chmod +x *
+./setup
 
 clear
 figlet -f mono12 "Pronto" | lolcat
