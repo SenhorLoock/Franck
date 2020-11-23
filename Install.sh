6a54b3e99cd91bbecfc7dac333fec4f3567dd47e
SenhorLoock <71995271+shoowl@users.noreply.github.com>
2020-11-23 14:14:32 -0300

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,15 @@
+clear 
+
+git clone https://github.com/shoowl/Auxiliar
+git clone https://github.com/sundowndev/PhoneInfoga
+git clone https://github.com/Cesar-Hack-Gray/SocialSploit
+git clone https://github.com/Cesar-Hack-Gray/webcam
+
+clear
+figlet -f mono12 "Pronto" | lolcat
+echo " Precione Enter Para Continuar "
+read -s enter
+
+rm -rf install.sh
+
+bash Franck.sh
