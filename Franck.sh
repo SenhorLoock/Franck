22c797d0c41d61b75399bbd9703e80634a4764c3
SenhorLoock <71995271+shoowl@users.noreply.github.com>
2020-11-23 14:01:08 -0300

    Create Franck.sh

---
+++ Franck.sh
@@ -0,0 +1,124 @@
+
+clear
+
+figlet -f mono12 "Franc" | lolcat
+date | lolcat
+
+echo "Ola Sou o Franck Fui Criado Para Ajuda a Obter Experiencia                 [*]De Enter Para Continua..." | lolcat
+    read -s nome
+     clear
+
+echo "Seja Bem Vindo" | lolcat
+
+
+cowsay -f dragon "Franck" | lolcat
+date | lolcat
+
+echo "[01]Salas Ngrok [02]Abrir SocialSploit [03]Abrir Auxiliar-msf              [04]Abrir PhoneInfoga [05]Abrir Webcam  [06]Criar Script                   [07]Execulta Script Criado [08]Git Clone [00]Exit" | lolcat
+read resultado
+
+if [ "$resultado" = "01" ]
+   then
+     clear
+
+        bash pastangrok.sh
+
+fi
+
+if [ "$resultado" = "02" ]
+    then
+      echo "Abrindo Ferramenta" | lolcat
+       cd SocialSploit
+        ./Sploit
+
+fi
+
+if [ "$resultado" = "03" ]
+    then
+
+cd Auxiliar
+
+bash Auxiliar.sh
+clear
+fi
+
+
+if [ "$resultado" = "04" ]
+  then
+cd PhoneInfoga
+figlet -f mono12 "Numero" | lolcat
+echo "Numero Exeplo +55999999999" | lolcat
+read numero
+python phoneinfoga.py -n $numero
+cd
+echo "Precione Enter Para Contina" | lolcat
+read enter
+bash Franck.sh
+fi
+
+if [ "$resultado" = "05" ]
+    then
+cd webcam
+./webcam
+fi
+
+if [ "$resultado" = "06" ]
+  then
+clear
+
+figlet -f mono12 "Fash" | lolcat
+
+echo "Qual Nome Do Arquivo Quer Abrir Senhor Loock?"
+read Nome
+
+cd storage/downloads/Loock/Scripts
+
+nano $nome
+
+fi
+
+if [ "$resultado" = "07" ]
+then
+clear
+
+figlet -f mono12 "cash" | lolcat
+
+echo "Qual Arquivo Deseja Abrir Senhor Loock ?"
+read execute
+
+cd storage/downloads/Loock/Scripts
+
+bash $execute
+
+fi
+
+if [ "$resultado" = "08" ]
+   then
+clear
+figlet -f mono12 "GitHub" | lolcat
+ echo "Digite a Url Do GitHub" | lolcat
+read url
+  git clone $url
+   figlet -f mono12 "Pronto" | lolcat
+  echo "Digite Enter Para Continua" | lolcat
+   clear
+    figlet -f mono12 "Pastas" | lolcat
+    ls
+    echo "Escolha o Local Que Deseja Abirir" | lolcat
+read pasta
+
+cd $pasta
+figlet -f mono12 "Lista" | lolcat
+
+ls
+  echo "Digite o comando Para iniciar a Farramenta:Ex: bash teste.sh.." | lolcat
+read comando
+$comando
+fi
+
+if [ "$resultado" = "00" ]
+   then
+clear
+echo "Fechando Assistente" | lolcat
+figlet -f mono12 "Fechado" | lolcat
+fi
