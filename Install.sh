7c163ecf056ab8778a7e6f802e4e04cfb53c5d83
SenhorLoock <71995271+shoowl@users.noreply.github.com>
2020-11-23 15:23:24 -0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -7,13 +7,18 @@ echo "Precione Enter Para Continua" | lolcat
 read -s Enter1
 cd Auxiliar
 bash install.sh
+cd ..
 
 clear
 figlet -f mono12 "2Etapa" | lolcat
 
-git clone https://github.com/sundowndev/PhoneInfoga
+git clone https://github.com/userx-dev/Phoneinfoga
 echo "Precione Enter Para Continuar" | lolcat
 read-s Enter2
+cd Phoneinfoga
+python3 -m pip install -r requirements.txt
+cd ..
+
 
 clear
 figlet -f mono12 "2Etapa" | lolcat
@@ -23,6 +28,7 @@ read -s Enter3
 cd SocialSploit 
 
 bash install.sh
+cd..
 
 clear
 figlet -f mono12 "4Etapa" | lolcat
@@ -31,12 +37,13 @@ echo "Precione Enter Para Continuar" | lolcat
 read -s Enter4
 chmod +x *
 ./setup
+cd ..
 
 clear
 figlet -f mono12 "Pronto" | lolcat
 echo " Precione Enter Para Continuar "
 read -s enter
 
-rm -rf install.sh
+rm -rf Install.sh
 
 bash Franck.sh
